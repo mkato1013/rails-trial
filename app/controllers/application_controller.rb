d22cce0521611d01a9c6fab3cb588204bbba87ad
m.kato <mkatochoco1013@gmail.com>
2023-02-28 16:24:01 +0900

    [Add] newアクション、createアクション、view作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  # サインイン後遷移先
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  # サインアウト後遷移先
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
--- app/controllers/sweets_controller.rb
+++ app/controllers/sweets_controller.rb
@@ -1,6 +1,39 @@
 class SweetsController < ApplicationController
-
   def index
-    logger.debug('warehsd')
+    @sweets = Sweet.all
+    # binding.break //ログ出力
+    # binding.pry
+  end
+
+  def new
+    @sweet = Sweet.new
+  end
+
+  def create
+    @sweet = Sweet.new(sweet_params)
+    @sweet.user_id = current_user.id
+    if @sweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    logger.debug("rseges")
+  end
+
+  def update
+    logger.debug("rseges")
+  end
+
+  def delete
+    logger.debug("rseges")
+  end
+
+  private
+
+  def sweet_params #ストロングパラメータ
+    params.require(:sweet).permit(:name, :description) #パラメーターのキー
   end
 end
